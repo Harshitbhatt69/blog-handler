379fff76b01041b1cc028786666205034346b9f9
Harshit B <harshitbhatt972@gmail.com>
2020-09-26 16:30:10 +0530

    added blog post number and a button in home screen with useState and a call back function

--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import React , {useContext} from 'react';
+import {Text, View, StyleSheet, FlatList, Button} from 'react-native';
+import BlogContext from '../context/BlogContext'
+
 
 const IndexScreen = () => {
-    return (
+
+    const {data, addBlogPost} = useContext(BlogContext);
+
+    return (    //onpress can be like {() => addBlogPost()} also
         <View>
         <Text>Index Screen</Text>
+        <Button title= "Add Posts" onPress = {addBlogPost}/>    
+        <FlatList
+        data = {data}
+        keyExtractor = {blogPost => blogPost.title}
+        renderItem = {({ item }) => {
+            return <Text>{item.title}</Text>;
+        }}
+        />
         </View>
     );
 };
