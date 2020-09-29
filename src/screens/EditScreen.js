d04fbc7cbd6fee47ee0e9cfb67707881a176b30f
Harshit B <harshitbhatt972@gmail.com>
2020-09-29 20:43:26 +0530

    extracted form logic from creatscreen and apply in edit screen and createscreen and named the hook file as blogpostform

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,31 +1,15 @@
 import React, { useState, useContext } from "react";
-import { View, Text, StyleSheet, TextInput } from "react-native";
-import { Context, context } from "../context/BlogContext";
+import { StyleSheet } from "react-native";
+import BlogPostForm from "../components/BlogPostForm";
+import { Context } from "../context/BlogContext";
 
 const EditScreen = ({ navigation }) => {
   const { state } = useContext(Context);
   const blogPost = state.find(
     (blogPost) => blogPost.id === navigation.getParam("id")
   );
-  const [title, setTitle] = useState(blogPost.title);
-  const [content, setContent] = useState(blogPost.content);
 
-  return (
-    <View>
-      <Text style={styles.label}>Edit Titel:</Text>
-      <TextInput
-        style={styles.input}
-        value={title}
-        onChangeText={(newTitle) => setTitle(newTitle)}
-      />
-      <Text style={styles.label}>Edit Content:</Text>
-      <TextInput
-        style={styles.input}
-        value={content}
-        onChangeText={(newContent) => setContent(newContent)}
-      />
-    </View>
-  );
+  return <BlogPostForm />;
 };
 
 const styles = StyleSheet.create({
