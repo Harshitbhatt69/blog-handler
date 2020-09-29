ad483961fa000596a9add74acbdc101bfb13109e
Harshit B <harshitbhatt972@gmail.com>
2020-09-30 02:12:41 +0530

    created a new const of edit blog post for dispatch function. Upnext will be creating a new testcase inside reducer

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -4,16 +4,15 @@ import BlogPostForm from "../components/BlogPostForm";
 import { Context } from "../context/BlogContext";
 
 const EditScreen = ({ navigation }) => {
-  const { state } = useContext(Context);
-  const blogPost = state.find(
-    (blogPost) => blogPost.id === navigation.getParam("id")
-  );
+  const id = navigation.getParam("id");
+  const { state, editBlogPost } = useContext(Context);
+  const blogPost = state.find((blogPost) => blogPost.id === id);
 
   return (
     <BlogPostForm
       initialValues={{ title: blogPost.title, content: blogPost.content }}
       onSubmit={(title, content) => {
-        console.log(title, content);
+        editBlogPost(id, title, content);
       }}
     />
   );
